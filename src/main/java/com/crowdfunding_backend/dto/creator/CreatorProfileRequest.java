2897333a7d459a6bed56615b010d30a0fe2da348
isatishsingh <chauhansatish978@gmail.com>
2026-04-30 00:57:47 +0530

    implemented validation at creatorProfileRequest

--- src/main/java/com/crowdfunding_backend/dto/creator/CreatorProfileRequest.java
+++ src/main/java/com/crowdfunding_backend/dto/creator/CreatorProfileRequest.java
@@ -1,11 +1,27 @@
 package com.crowdfunding_backend.dto.creator;
+import jakarta.validation.constraints.*;
 
 public class CreatorProfileRequest {
 
+  @NotBlank(message = "Aadhaar is required")
+  @Pattern(regexp = "^\\d{12}$", message = "Aadhaar must be 12 digits")
   private String aadhaarNumber;
+
+  @NotBlank(message = "PAN is required")
+  @Pattern(regexp = "^[A-Z]{5}[0-9]{4}[A-Z]{1}$",
+           message = "Invalid PAN format")
   private String panNumber;
+
+  @Pattern(regexp = "^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$",
+           message = "Invalid GST number")
+
   private String gstNumber;
+
+  @Pattern(regexp = "^[A-Z][0-9]{7}$", message = "Invalid passport number")
   private String passportNumber;
+
+  @NotBlank(message = "Phone number is required")
+  @Pattern(regexp = "^[6-9]\\d{9}$", message = "Invalid Indian phone number")
   private String phoneNumber;
 
   // ✅ GETTERS
